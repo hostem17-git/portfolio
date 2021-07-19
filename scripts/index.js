f9a7711949ca6002628ccc3626e2c7674f23f880
hostem17 <vjjais17@gmail.com>
2021-07-19 22:49:50 +0530

    redoing about

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,8 @@ function remove() {
 }
 
 var heroSlideIns = document.querySelectorAll(".heroName .slideIn")
-var aboutSlideIns = document.querySelectorAll(".aboutMe .slideInText")
-var pics = document.querySelectorAll(".picContanier")
+var aboutSlideIns = document.querySelectorAll(".aboutMe .slideIn")
+var pics = document.querySelectorAll(".pic")
 var skillRows = document.querySelectorAll(".skills .slideIn")
 var skillDiv = document.querySelectorAll(".skillContent")
 var projectDiv = document.querySelectorAll(".project .slideIn")
@@ -29,6 +29,8 @@ function debounce(func, wait = 10, immediate = true) {
 };
 
 function checkSlide() {
+    test3();
+
     heroSlideIns.forEach(ele => {
         // half way through the image
         const slideInAt = (window.scrollY + window.innerHeight + ele.clientHeight / 2);
@@ -45,7 +47,7 @@ function checkSlide() {
 
     aboutSlideIns.forEach(ele => {
         // half way through the image
-        const slideInAt = (window.scrollY + 3 * window.innerHeight / 4 + ele.clientHeight / 2);
+        const slideInAt = (window.scrollY + window.innerHeight + ele.clientHeight / 2);
         // bottom of the image
         const divBottom = ele.offsetTop + ele.clientHeight;
         const isHalfShown = slideInAt > ele.offsetTop;
@@ -60,38 +62,36 @@ function checkSlide() {
 
     pics.forEach(ele => {
         // half way through the image
-        const slideInAt = (window.scrollY + window.innerHeight + 3 * ele.clientHeight / 4);
+        const slideInAt = (window.scrollY + 3 * window.innerHeight / 4 + ele.clientHeight / 2);
         // bottom of the image
         const divBottom = ele.offsetTop + ele.clientHeight;
         const isHalfShown = slideInAt > ele.offsetTop;
         const isNotScrolledPast = window.scrollY < divBottom;
-
+        console.log(slideInAt, divBottom, isHalfShown, isNotScrolledPast)
         if (isHalfShown && isNotScrolledPast) {
-            ele.classList.add('picActive');
+            ele.classList.add('active');
         } else {
-            ele.classList.remove('picActive');
+            ele.classList.remove('active');
         }
 
     });
 
 
-    skillRows.forEach(ele => {
-        // half way through the image
-        const slideInAt = (window.scrollY + window.innerHeight / 2 + ele.clientHeight / 2);
-        // bottom of the image
-        const divBottom = ele.offsetTop + ele.clientHeight;
-        const isHalfShown = slideInAt > ele.offsetTop;
-        const isNotScrolledPast = window.scrollY < divBottom;
-        console.log(isHalfShown, " ", isNotScrolledPast);
-        if (isHalfShown && isNotScrolledPast) {
-            ele.classList.add("active");
-        } else {
-            ele.classList.remove("active");
-        }
-    });
+    // skillRows.forEach(ele => {
+    //     // half way through the image
+    //     const slideInAt = (window.scrollY + window.innerHeight / 2 + ele.clientHeight / 2);
+    //     // bottom of the image
+    //     const divBottom = ele.offsetTop + ele.clientHeight;
+    //     const isHalfShown = slideInAt > ele.offsetTop;
+    //     const isNotScrolledPast = window.scrollY < divBottom;
+    //     if (isHalfShown && isNotScrolledPast) {
+    //         ele.classList.add("active");
+    //     } else {
+    //         ele.classList.remove("active");
+    //     }
+    // });
 
 
-    /*
     skillDiv.forEach(ele => {
         // half way through the image
         const slideInAt = (window.scrollY + window.innerHeight / 2 + ele.clientHeight / 2);
@@ -109,7 +109,7 @@ function checkSlide() {
                 row.classList.remove("active");
             });
         }
-    });*/
+    })
 
     projectDiv.forEach(ele => {
         // half way through the image
@@ -118,7 +118,6 @@ function checkSlide() {
         const divBottom = ele.offsetTop + ele.clientHeight;
         const isHalfShown = slideInAt > ele.offsetTop;
         const isNotScrolledPast = window.scrollY < divBottom;
-        console.log(isHalfShown, " ", isNotScrolledPast);
         if (isHalfShown && isNotScrolledPast) {
             ele.classList.add("active");
         } else {
@@ -130,6 +129,39 @@ function checkSlide() {
 }
 
 
+function test3() {
+    if (window.scrollY >= 200)
+        test();
+    else {
+        test2();
+    }
+}
+
+function test() {
+    var ele = document.querySelector(".connectButton");
+
+    ele.style.borderRadius = "100%";
+    ele.style.top = "90%";
+    ele.style.left = "95%";
+    ele.style.width = "50px";
+    ele.style.height = "50px";
+    ele.innerHTML = "M";
+    ele.style.position = "fixed";
+}
+
+function test2() {
+    var ele = document.querySelector(".connectButton");
+    ele.style.borderRadius = "5%";
+    ele.style.top = "75%";
+    ele.style.left = "45%";
+    ele.style.width = "100px";
+    ele.style.height = "35px";
+    ele.innerHTML = "Lets Connect";
+    ele.style.position = "fixed";
+
+}
+
 window.addEventListener('scroll', debounce(checkSlide));
 
-checkSlide()
+
+checkSlide();
